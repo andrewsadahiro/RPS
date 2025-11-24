3d9d8758e582b278877cbe55c437f16a77eb303d
andrewsadahiro <136555256+andrewsadahiro@users.noreply.github.com>
2025-11-24 13:56:06 -0800

    LCD implemented

--- rps.py
+++ rps.py
@@ -69,7 +69,66 @@ def tie_sfx():
 
 
 
+# === LCD ===
+LCD_RS = OutputDevice(25)
+LCD_E = OutputDevice(24)
+LCD_D4 = OutputDevice(23)
+LCD_D5 = OutputDevice(18)
+LCD_D6 = OutputDevice(15)
+LCD_D7 = OutputDevice(14)
 
+LCD_WIDTH = 16
+LCD_CHR = True
+LCD_CMD = False
+LCD_LINE_1 = 0x80
+LCD_LINE_2 = 0xC0
+
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+def lcd_init():
+    lcd_byte(0x33, LCD_CMD)
+    lcd_byte(0x32, LCD_CMD)
+    lcd_byte(0x28, LCD_CMD)
+    lcd_byte(0x0C, LCD_CMD)
+    lcd_byte(0x06, LCD_CMD)
+    lcd_byte(0x01, LCD_CMD)
+    sleep(E_DELAY)
+    
+def lcd_byte(bits, mode):
+    LCD_RS.value = mode
+
+    # HIGH nibble
+    LCD_D4.value = 1 if bits & 0x10 else 0
+    LCD_D5.value = 1 if bits & 0x20 else 0
+    LCD_D6.value = 1 if bits & 0x40 else 0
+    LCD_D7.value = 1 if bits & 0x80 else 0
+    lcd_toggle_enable()
+
+    # LOW nibble
+    LCD_D4.value = 1 if bits & 0x01 else 0
+    LCD_D5.value = 1 if bits & 0x02 else 0
+    LCD_D6.value = 1 if bits & 0x04 else 0
+    LCD_D7.value = 1 if bits & 0x08 else 0
+    lcd_toggle_enable()
+    
+def lcd_toggle_enable():
+    sleep(E_DELAY)
+    LCD_E.on()
+    sleep(E_PULSE)
+    LCD_E.off()
+    sleep(E_DELAY)
+    
+def lcd_string(message, line):
+    message = message.ljust(LCD_WIDTH, " ")
+    
+    lcd_byte(line, LCD_CMD)
+    
+    for i in range(LCD_WIDTH):
+        lcd_byte(ord(message[i]), LCD_CHR)
+        
+lcd_init()
+# ===========
 
 
 
@@ -186,14 +245,20 @@ def game_conc(outcome):
     if outcome == "Tie":
         print("Tie")
         tie_sfx()
+        lcd_string("   ITS A", LCD_LINE_1)
+        lcd_string("    TIE", LCD_LINE_2)
     elif outcome == "Win":
         print("You Win")
         p_score += 1
         win_sfx()
+        lcd_string("      YOU", LCD_LINE_1)
+        lcd_string("      WIN", LCD_LINE_2)
     elif outcome == "Lose":
         print("You Lose")
         b_score += 1
         lose_sfx()
+        lcd_string("      YOU", LCD_LINE_1)
+        lcd_string("      LOSe", LCD_LINE_2)
     print(f'\n    ==Score Board==\nPlayer = {p_score} | Computer = {b_score}\n')
     
 # main game function
